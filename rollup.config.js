04c7383701182c5b65f620cc76e76802ab7e2ad0
yangchen62 <462428585@qq.com>
2020-08-27 10:50:59 +0800

    测试

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const babel = require('rollup-plugin-babel');
 const {nodeResolve} = require('@rollup/plugin-node-resolve');
+const uglify = require('rollup-plugin-uglify').uglify;
+const merge = require('lodash.merge');
 const pkg = require('./package.json');
 
 const extensions = ['.js', '.ts'];
@@ -9,20 +11,48 @@ const resolve = function(...args) {
     return path.resolve(__dirname, ...args);
 };
 
-module.exports = {
-    input: resolve('./src/index.ts'),
-    output: {
-        file: resolve('./', pkg.main), // 为了项目的统一性，这里读取 package.json 中的配置项
-        format: 'esm',
+// 打包任务的个性化配置
+const jobs = {
+    esm: {
+        output: {
+            format: 'esm',
+            file: resolve(pkg.module),
+        },
     },
-    plugins: [
-        nodeResolve({
-            extensions,
-            modulesOnly: true
-        }),
-        babel({
-            exclude: 'node_modules/**',
-            extensions,
-        }),
-    ],
+    umd: {
+        output: {
+            format: 'umd',
+            file: resolve(pkg.main),
+            name: 'rem',
+        },
+    },
+    min: {
+        output: {
+            format: 'umd',
+            file: resolve(pkg.main.replace(/(.\w+)$/, '.min$1')),
+            name: 'rem',
+        },
+        plugins: [uglify()],
+    },
 };
+
+// 从环境变量获取打包特征
+const mergeConfig = jobs[process.env.FORMAT || 'esm'];
+
+module.exports = merge(
+    {
+        input: resolve('./src/index.ts'),
+        output: {},
+        plugins: [
+            nodeResolve({
+                extensions,
+                modulesOnly: true,
+            }),
+            babel({
+                exclude: 'node_modules/**',
+                extensions,
+            }),
+        ],
+    },
+    mergeConfig,
+);
--- src/container/container.ts
+++ src/container/container.ts
@@ -1,3 +1,5 @@
 export class Container {
+    constructor(props: any) {
+    }
 
 }
